e8776dedd4e893a64231b06b3e8acb63933687a9
sneelyg <santiagoneely@gmail.com>
2023-06-01 19:51:33 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld 
+  msg="Howdy!"
+  bodyText=" Esto es un mensaje de prueba"
+  />
+  <FooterComponent message="Hola Ctmmm"></FooterComponent>
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FooterComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <footer>
+<p>{{message}}</p>
+  </footer>
+</template>
+<script>
+export default {
+    name: "FooterComponent",
+    props: {
+        message: String
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
